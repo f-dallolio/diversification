b20132c8c4077b76bc2c5ca639b470188d42285a
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-03-25 02:06:12 -0400

    added reframe_by_key

--- R/summarise_by_key.R
+++ R/summarise_by_key.R
@@ -13,10 +13,27 @@ summarise_by_key <- function(.data, ... , .and_by = NULL, .groups = NULL){
   stopifnot(".data must be a tsibble" = tsibble::is_tsibble(.data))
   tbl_data = tidyr::as_tibble(.data)
   keys <- key_vars(.data)
-  grps <- tbl_data %>% select(all_of(keys), {{.and_by}}) %>% names()
+  grps <- names(
+    tidyselect::eval_select(rlang::expr( c( keys, {{.and_by}}) ) , tbl_data)
+  )
   newdata <- dplyr::grouped_df(
     data = tidyr::as_tibble(.data),
     vars = grps
   )
   dplyr::summarise(.data = newdata, ... , .by = NULL)
+}
+
+#' @export
+reframe_by_key <- function(.data, ... , .and_by = NULL, .groups = NULL){
+  stopifnot(".data must be a tsibble" = tsibble::is_tsibble(.data))
+  tbl_data = tidyr::as_tibble(.data)
+  keys <- key_vars(.data)
+  grps <- names(
+    tidyselect::eval_select(rlang::expr( c( keys, {{.and_by}}) ) , tbl_data)
+  )
+  newdata <- dplyr::grouped_df(
+    data = tidyr::as_tibble(.data),
+    vars = grps
+  )
+  dplyr::reframe(.data = newdata, ... , .by = NULL)
 }

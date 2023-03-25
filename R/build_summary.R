aca115391d072c17d2e61cc63c7d7eb6c063a3d0
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-03-25 08:56:11 -0400

    .

--- R/build_summary.R
+++ R/build_summary.R
@@ -20,9 +20,6 @@ build_summary <- function(.data, .and_by, .cols, .fns){
     mutate(row = row_number()) %>%
     select(row, all_of(idx), all_of(keys), {{.and_by}}, {{.cols}} )
 
-  ts_attributes <- attributes(.data)
-
-
   tbl_data = as_tibble(ts_data)
 
 
@@ -36,32 +33,27 @@ build_summary <- function(.data, .and_by, .cols, .fns){
     grouped_df(vars = names(.)[-1])
 
   group_names <- group_data %>% select(-row) %>% names
-
-  group_attr <- group_data %>%
+  group_class <- group_data %>%
     select(-row) %>%
-    map( ~ list(class = class(.x), attributes = attributes(.x))) %>%
-    set_names(group_names)
+    map_chr(~ class(.x))
 
 
   var_data <- tbl_data %>%
     select( row,  {{.cols}} )
 
   var_names <- var_data %>% select(-row) %>% names
-
-  var_attr <- var_data %>%
+  var_class <- var_data %>%
     select(-row) %>%
-    map( ~ list(class = class(.x), attributes = attributes(.x))) %>%
-    set_names(var_names)
+    map_chr(~ class(.x))
+
 
   fun_list <- dots_list(.fns, .named = TRUE)
 
+
   list(
-    ts_list = c(list(data = ts_data),
-                ts_attributes),
-    group_list = list(names = group_names,
-                      attributes = group_attr),
-    var_list = list(names = var_names,
-                    attributes = var_attr),
+    ts_list = list(data = ts_data),
+    group_list = list(names = group_names, class = group_class),
+    var_list = list(names = var_names, class = var_class),
     fun_list = .fns )
 
 }
